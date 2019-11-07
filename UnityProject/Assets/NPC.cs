1b6fcd0deda2f69c2eb2f7a3a37b179881ff37f7
michaello363 <55518346+Michaello363@users.noreply.github.com>
2019-11-07 11:58:49 +0800

    期中完成

---
+++ UnityProject/Assets/NPC.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    [Header("對話")]
+    public string say = "hello";
+    public float sayspeed = 1.5f;
+    [Header("任務")]
+    public bool Compiete = false;
+    public int bateryfinish = 5;
+    public int batery = 0;
+
+}
---
+++ UnityProject/Assets/robot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot : MonoBehaviour
+{
+    [Header("速度")]
+    [Range(0, 100)]
+    public int speed = 50;
+    [Header("跳躍")]
+    [Range(0, 300)]
+    public int jump = 0;
+    [Header("是否在地上")]
+    public bool IsGround = false;
+
+    private Rigidbody r3d;
+    // Use this for initialization
+    private void Start()
+    {
+        r3d = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Jump();
+        Walk();
+    }
+    private void Walk()
+    {
+        r3d.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0, speed * Input.GetAxis("Vertical")));
+    }
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && IsGround == true)
+        {
+            IsGround = false;
+            r3d.AddForce(new Vector3(0, jump, 0));
+        }
+    }
+}
